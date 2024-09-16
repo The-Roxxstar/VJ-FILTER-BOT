2c72be87a2306b6ed0ec92373d3be6349640b38c
The-Roxxstar <followinstagram296@gmail.com>
2024-09-16 11:44:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -139,7 +139,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://clever-malvina-rox-467cb415.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
