24b4f1ccade10861cedc1f9f8d36d36b77f0562e
The-Roxx <messytalkshorts@gmail.com>
2024-09-28 15:33:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://MANISH:MANISH@cluster0.523dr0f.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "manish")
+DATABASE_NAME = environ.get('DATABASE_NAME', "techvjcollection")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 # Premium And Referal Settings
